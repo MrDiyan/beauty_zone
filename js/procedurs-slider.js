358b800e302a0639c2aa765717858c4851de02a8
MrDiyan <138104482+MrDiyan@users.noreply.github.com>
2024-01-09 12:09:31 +0200

    added main page

---
+++ js/procedurs-slider.js
@@ -0,0 +1,24 @@
+const swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 25,
+  
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+    },
+    1280: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.pagination',
+    clickable: false,
+    bulletClass: 'swiper-pagination-bullet',
+    bulletActiveClass: 'swiper-pagination-bullet-active',
+  },
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
+});
